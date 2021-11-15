d991ed8851eace2c80f03c85913c904554abfd65
muskatnussWeb <93988600+muskatnussWeb@users.noreply.github.com>
2021-11-15 23:38:41 +0100

    Update fragmentShader.js

--- javascript/shaders/fragmentShader.js
+++ javascript/shaders/fragmentShader.js
@@ -8,7 +8,7 @@
     uniform sampler2D uSampler;
 
     void main() {
-      highp vec4 texelColor = texture2D(uSampler, vtextureCoord);
+      highp vec4 texelColor = texture2D(uSampler, vTextureCoord);
 
       gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
     }
