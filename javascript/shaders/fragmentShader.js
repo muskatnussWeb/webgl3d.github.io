061c2b0f844812620e6d0c6cc788ac7eeb6eb802
muskatnussWeb <93988600+muskatnussWeb@users.noreply.github.com>
2021-11-15 22:57:30 +0100

    Add files via upload

---
+++ javascript/shaders/fragmentShader.js
@@ -0,0 +1,17 @@
+ // Fragment shader program
+
+  const fsSource = `
+  
+    varying highp vec2 vTextureCoord;
+    varying highp vec3 vLighting;
+
+    uniform sampler2D uSampler;
+
+    void main() {
+      highp vec4 texelColor = texture2D(uSampler, vtextureCoord);
+
+      gl_FragColor = texture2D(uSampler, vTextureCoord);
+    }
+`;
+
+export {fsSource};
