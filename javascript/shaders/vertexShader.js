29378ce5c2395881a54a91dbf5f91930beb80f81
muskatnussWeb <93988600+muskatnussWeb@users.noreply.github.com>
2021-11-15 21:28:15 +0100

    Update vertexShader.js

--- javascript/shaders/vertexShader.js
+++ javascript/shaders/vertexShader.js
@@ -7,7 +7,7 @@
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
 
-    varying lowp vec2 vTextureCoord;
+    varying highp vec2 vTextureCoord;
 
     void main() {
       gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
